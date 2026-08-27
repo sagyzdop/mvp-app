5c82ce95ce1fe1ee8eda191592c89d7667fd30d2
sagyzdop <sagyzdop@gmail.com>
2026-08-27 15:22:25 +0500

    fix: remove unnecessary scope from Google auth configuration

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -12,7 +12,6 @@ export const auth = betterAuth({
   socialProviders: {
     google: {
       prompt: 'select_account',
-      scope: ['openid', 'email', 'profile', 'https://www.googleapis.com/auth/calendar.events'],
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
